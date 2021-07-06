15c6d9bae1a0e57f1bae896c7e030d395de81315
Vresod <lukebinkofsky@gmail.com>
2021-07-06 11:50:13 -0400

    add option to dump after each group of messages this saves memory if it is enabled

--- main.py
+++ main.py
@@ -17,6 +17,9 @@ config['BOT'] = True if config['TOKEN'].startswith('Bot ') else config['BOT']
 
 STARTPOINT = "https://discord.com/api/v9"
 
+WANTED_ATTRS = {'content','author','id','timestamp','edited_timestamp','attachments','embeds','reactions','pinned','type','referenced_message'}
+LIMIT = 100
+
 def request_endpoint(endpoint,method:RequestType=RequestType.GET,data:dict={}):
 	"""
 	gets an endpoint in form "/users/@me"
@@ -24,6 +27,46 @@ def request_endpoint(endpoint,method:RequestType=RequestType.GET,data:dict={}):
 	if method == RequestType.GET: return json.loads(requests.get(STARTPOINT + endpoint,headers={'Authorization':token}).text)
 	elif method == RequestType.POST: return json.loads(requests.post(STARTPOINT + endpoint,headers={'Authorization':token},data=data).text)
 	else: raise ValueError(f'Unsupported method {method.name!r}')
+
+def dump_json_at_end():
+	messages = []
+	oldest_snowflake:int = 0
+	LIMIT = 100
+	while True:
+		now = datetime.now()
+		time = now.strftime("%H:%M:%S")
+		current_messages:list = request_endpoint(f'/channels/{channel}/messages?limit={LIMIT}&{"before=" + oldest_snowflake if oldest_snowflake else ""}')
+		for message in current_messages:
+			temp_message = {}
+			for i in WANTED_ATTRS:
+				temp_message[i] = message.get(i)
+			print(f"[{time}] <{message['author']['username']}#{message['author']['discriminator']}>: {message['content']}")
+			oldest_snowflake = message['id']
+		if len(current_messages) != LIMIT:
+			break
+	messages.reverse()
+	with open("dump.json","w") as dumpfile: json.dump(messages,dumpfile)
+
+def dump_json_for_each():
+	WANTED_ATTRS = {'content','author','id','timestamp','edited_timestamp','attachments','embeds','reactions','pinned','type','referenced_message'}
+	oldest_snowflake:int = 0
+	with open('dump.json','w') as dumpfile: dumpfile.write('[')
+	dumpfile = open('dump.json','a')
+	while True:
+		messages = []
+		current_messages:list = request_endpoint(f'/channels/{channel}/messages?limit={LIMIT}&{"before=" + oldest_snowflake if oldest_snowflake else ""}')
+		for message in current_messages:
+			temp_message = {}
+			for i in WANTED_ATTRS:
+				temp_message[i] = message.get(i)
+			messages.append(temp_message)
+		for message in messages:
+			dumpfile.write(json.dumps(message))
+			if message != messages[-1]: dumpfile.write(',')
+		if len(current_messages) != LIMIT:
+			break
+	dumpfile.write(']')
+
 
 if __name__ == "__main__":
 	user = request_endpoint("/users/@me")
@@ -33,21 +76,7 @@ if __name__ == "__main__":
 	except ValueError:
 		print('No channel provided, exiting program')
 		exit(1)
-	messages = []
-	oldest_snowflake:int = 0
-	LIMIT = 100
-	wanted_attrs = {'content','author','id','timestamp','edited_timestamp','attachments','embeds','reactions','pinned','type','referenced_message'}
-	while True:
-		now = datetime.now()
-		time = now.strftime("%H:%M:%S")
-		current_messages:list = request_endpoint(f'/channels/{channel}/messages?limit={LIMIT}&{"before=" + oldest_snowflake if oldest_snowflake else ""}')
-		for message in current_messages:
-			temp_message = {}
-			for i in wanted_attrs:
-				temp_message[i] = message.get(i)
-			print(f"[{time}] <{message['author']['username']}#{message['author']['discriminator']}>: {message['content']}")
-			oldest_snowflake = message['id']
-		if len(current_messages) != LIMIT:
-			break
-	messages.reverse()
-	with open("dump.json","w") as dumpfile: json.dump(messages,dumpfile)
+	if config['DUMP_AT_END'] == 'True':
+		dump_json_at_end()
+	else:
+		dump_json_for_each()
