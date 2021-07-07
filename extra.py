e4d76965361954abfef1b459d81864d92aaa9d9d
Vresod <lukebinkofsky@gmail.com>
2021-07-07 19:20:26 -0400

    add more http verbs to RequestType

--- extra.py
+++ extra.py
@@ -1,4 +1,4 @@
-from enum import IntEnum as IntEnum
+from enum import IntEnum
 
 class RequestType(IntEnum): # make an enum of request types; this probably already exists in requests but i couldn't find it
 	GET = 1
@@ -6,3 +6,7 @@ class RequestType(IntEnum): # make an enum of request types; this probably alrea
 	# we dont need the rest of the types but we'll have them anyways
 	PATCH = 3
 	DELETE = 4
+	HEAD = 5
+	OPTIONS = 6
+	PUT = 7
+	TRACE = 8
