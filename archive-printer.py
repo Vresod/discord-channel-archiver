bca3481e465c14b3142db905a1d62675ef685b31
Vresod <lukebinkofsky@gmail.com>
2021-07-08 00:56:51 -0400

    add small script to print dumps

---
+++ archive-printer.py
@@ -0,0 +1,14 @@
+import argparse
+import json
+from datetime import datetime
+
+parser = argparse.ArgumentParser(description='Prints Discord DMs dumped using discord-channel-archiver')
+parser.add_argument('filename',help='the file that is parsed')
+args = parser.parse_args()
+
+messages:list = json.load(open(args.filename))
+messages.sort(key=lambda msg: int(msg['id']))
+
+for message in messages:
+	time = datetime.fromtimestamp(((int(message['id'])>> 22) + 1420070400000) / 1000).strftime("%x %X")
+	print(f"[{time}] <{message['author']['username']}#{message['author']['discriminator']}>: {message['content']}") # print in a format that changes depending on your region
