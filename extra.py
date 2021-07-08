6fda76eae8904915c2c57463b3404b3922c09a0e
Vresod <lukebinkofsky@gmail.com>
2021-07-07 22:48:19 -0400

    major restructuring and argparse

--- extra.py
+++ extra.py
@@ -1,4 +1,13 @@
 from enum import IntEnum
+from datetime import datetime
+from typing import Union
+from os import PathLike
+import requests
+import json
+
+STARTPOINT = "https://discord.com/api/v9"
+
+__all__ = ['RequestType','RequestClass','dump_json']
 
 class RequestType(IntEnum): # make an enum of request types; this probably already exists in requests but i couldn't find it
 	GET = 1
@@ -10,3 +19,45 @@ class RequestType(IntEnum): # make an enum of request types; this probably alrea
 	OPTIONS = 6
 	PUT = 7
 	TRACE = 8
+
+class RequestClass:
+	def __init__(self,token): self.token = token
+	def __call__(self, endpoint,method:RequestType=RequestType.GET,data:dict={}):
+		"""
+		gets an endpoint in form "/users/@me". only GET and POST are supported
+		"""
+		if method == RequestType.GET: return json.loads(requests.get(STARTPOINT + endpoint,headers={'Authorization':self.token}).text)
+		elif method == RequestType.POST: return json.loads(requests.post(STARTPOINT + endpoint,headers={'Authorization':self.token},data=data).text)
+		# only GET and POST are supported
+		else: raise ValueError(f'Unsupported method {method.name!r}')
+
+
+def dump_json(channel:Union[int,str],filename:PathLike,config:dict,LIMIT,WANTED_ATTRS,request_endpoint:RequestClass):
+	"""
+	dumps every message in a discord channel to filename
+	"""
+	oldest_snowflake:int = 0
+	dump_at_end = config['DUMP_AT_END'] == 'True' # str -> bool
+	file_mode = 'w' if dump_at_end else 'a' # write if dump at end, else append
+	with open('dump.json','w') as dumpfile: dumpfile.write('[' if not dump_at_end else '') # make the file and be sure its empty
+	dumpfile = open('dump.json',file_mode)
+	messages = []
+	while True:
+		time = datetime.now().strftime("%H:%M:%S")
+		if not dump_at_end: messages = [] # empty the messages list if dump_at_end
+		current_messages:list = request_endpoint(f'/channels/{channel}/messages?limit={LIMIT}&{"before=" + oldest_snowflake if oldest_snowflake else ""}')
+		for message in current_messages:
+			temp_message = {}
+			for i in WANTED_ATTRS:
+				temp_message[i] = message.get(i)
+			messages.append(temp_message)
+			print(f"[{time}] <{message['author']['username']}#{message['author']['discriminator']}>: {message['content']}") # print in format "[00:00:00] <DiscordUser#0001> Sample text"
+		# do weird lower-level json shenanigans if dump at end
+		if not dump_at_end:
+			for message in messages:
+				dumpfile.write(json.dumps(message))
+				if message != messages[-1]: dumpfile.write(',')
+		if len(current_messages) < LIMIT: # stop looping if this is the last one, ie we got less than the limit
+			break
+	if dump_at_end: json.dump(messages,dumpfile) # write all messages to the file at the end
+	else: dumpfile.write(']') # finish off the json array we've been creating the whole time
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-import json
-import requests
 import dotenv # allows a convenient config format
-from datetime import datetime # needed for verbosity
-from extra import RequestType # enum for requests
-from typing import Union
-from io import TextIOWrapper
-from os import PathLike
-from sys import argv as args # arguments
+from extra import RequestClass
+from extra import dump_json 
+import argparse
+
+parser = argparse.ArgumentParser(description='Archive discord channels')
+parser.add_argument("channel",help="channel to be archived",type=int)
+parser.add_argument("--token",'-t',help="token to be used")
+parser.add_argument("--bot",'-b',help="token to be used",action='store_true')
+args = parser.parse_args()
 
 # dotenv config
 config = {
@@ -23,53 +24,10 @@ STARTPOINT = "https://discord.com/api/v9"
 # stuff we don't throw away about the messages
 WANTED_ATTRS = {'content','author','id','timestamp','edited_timestamp','attachments','embeds','reactions','pinned','type','referenced_message'}
 LIMIT = 100 # amount of messages grabbed per request
-
-def request_endpoint(endpoint,method:RequestType=RequestType.GET,data:dict={}):
-	"""
-	gets an endpoint in form "/users/@me". only GET and POST are supported
-	"""
-	if method == RequestType.GET: return json.loads(requests.get(STARTPOINT + endpoint,headers={'Authorization':token}).text)
-	elif method == RequestType.POST: return json.loads(requests.post(STARTPOINT + endpoint,headers={'Authorization':token},data=data).text)
-	# only GET and POST are supported
-	else: raise ValueError(f'Unsupported method {method.name!r}')
-
-
-def dump_json(channel:Union[int,str],filename:PathLike):
-	"""
-	dumps every message in a discord channel to filename
-	"""
-	oldest_snowflake:int = 0
-	dump_at_end = config['DUMP_AT_END'] == 'True' # str -> bool
-	file_mode = 'w' if dump_at_end else 'a' # write if dump at end, else append
-	with open('dump.json','w') as dumpfile: dumpfile.write('[' if not dump_at_end else '') # make the file and be sure its empty
-	dumpfile = open('dump.json',file_mode)
-	messages = []
-	while True:
-		time = datetime.now().strftime("%H:%M:%S")
-		if not dump_at_end: messages = [] # empty the messages list if dump_at_end
-		current_messages:list = request_endpoint(f'/channels/{channel}/messages?limit={LIMIT}&{"before=" + oldest_snowflake if oldest_snowflake else ""}')
-		for message in current_messages:
-			temp_message = {}
-			for i in WANTED_ATTRS:
-				temp_message[i] = message.get(i)
-			messages.append(temp_message)
-			print(f"[{time}] <{message['author']['username']}#{message['author']['discriminator']}>: {message['content']}") # print in format "[00:00:00] <DiscordUser#0001> Sample text"
-		# do weird lower-level json shenanigans if dump at end
-		if not dump_at_end:
-			for message in messages:
-				dumpfile.write(json.dumps(message))
-				if message != messages[-1]: dumpfile.write(',')
-		if len(current_messages) < LIMIT: # stop looping if this is the last one, ie we got less than the limit
-			break
-	if dump_at_end: json.dump(messages,dumpfile) # write all messages to the file at the end
-	else: dumpfile.write(']') # finish off the json array we've been creating the whole time
-
 if __name__ == "__main__":
+	request_endpoint = RequestClass(token)
 	user = request_endpoint("/users/@me")
 	print(f'Logged in as {user.get("username")}#{user.get("discriminator")}')
-	try: channel = int(args[1])
-	except ValueError: # tell the user they messed up and quit
-		print('No channel provided, exiting program')
-		exit(1)
+	channel = args.channel
 	print(f"Dumping channel #{request_endpoint(f'/channels/{channel}')['name']}") # tell the user what channel they're dumping by name, to make sure they got the correct one. also nested f-strings lol
-	dump_json(channel,'dump.json')
+	dump_json(channel,'dump.json',config,LIMIT,WANTED_ATTRS,request_endpoint)
